15c572bdd6288917afdb2dbc7207250258b6cac5
John <patrick.meyerin@gmail.com>
2024-05-25 23:33:45 -0500

    starting over in micronaut

--- buildAndDeploy.sh
+++
@@ -1,3 +0,0 @@
-./gradlew clean bootJar
-docker build -t protocol-sn-node-manager:latest .
-docker compose up
--- src/main/resources/db/migration/V20230809__first_migration.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS plugins;
-
-CREATE TABLE IF NOT EXISTS plugins (
-    id SERIAL PRIMARY KEY,
-    pluginId varchar(256),
-    pluginName varchar(256),
-    baseUri varchar(1024)
-);
